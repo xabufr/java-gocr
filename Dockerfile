46fe34ba3883e2d2cf8b65cef5b3829821d61e15
Thomas Loubiou <t.loubiou@systonic.fr>
2016-08-11 15:45:46 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM java:8
+
+ADD install.sh /
+RUN /install.sh
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y build-essential
+mkdir work
+cd work
+wget http://www-e.uni-magdeburg.de/jschulen/ocr/gocr-0.50.tar.gz
+tar -xf gocr-0.50.tar.gz
+cd gocr-0.50
+./configure
+make install
+
+apt-get purge -y build-essential
+apt-get autoremove -y --purge build-essential
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
